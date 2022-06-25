20fa82477a8f2113bde86d02109b05d948d30365
Shardul <kumbharshardul384@gmail.com>
2022-06-25 14:44:14 +0530

    Attempeted 4 Balls challenge

--- src/main/java/TryProcessing.java
+++ src/main/java/TryProcessing.java
@@ -1,41 +1,34 @@
 import processing.core.PApplet;
 
 public class TryProcessing extends PApplet {
+    
     public static final int HEIGHT = 750;
     public static final int WIDTH = 950;
-    public static final int CIRCLE_DIA = 20;
-    public static final float ballAtY1 = HEIGHT * 1 / 5;
-    public static final float ballAtY2 = HEIGHT * 2 / 5;
-    public static final float ballAtY3 = HEIGHT * 3 / 5;
-    public static final float ballAtY4 = HEIGHT * 4 / 5;
-    private float ballAtX1 = 0;
-    private float ballAtX2 = 0;
-    private float ballAtX3 = 0;
-    private float ballAtX4 = 0;
+    public static final int DIAMETER = 20;
+    public static int position = 0;
+
 
     public static void main(String[] args) {
         PApplet.main("TryProcessing", args);
+
 
     }
 
     @Override
     public void settings() {
+        super.settings();
         size(WIDTH, HEIGHT);
     }
 
     @Override
     public void draw() {
-        getCirce(ballAtX1, ballAtY1);
-        getCirce(ballAtX2, ballAtY2);
-        getCirce(ballAtX3, ballAtY3);
-        getCirce(ballAtX4, ballAtY4);
-        ballAtX1 += 1;
-        ballAtX2 += 2;
-        ballAtX3 += 3;
-        ballAtX4 += 4;
+        for (int speed = 1; speed <= 4; speed++) {
+            ball(speed * position, speed * HEIGHT / 5);
+        }
+        position += 1;
     }
 
-    private void getCirce(float ballAtX, float ballAtY) {
-        ellipse(ballAtX, ballAtY, CIRCLE_DIA, CIRCLE_DIA);
+    private void ball(int positionX, int height) {
+        ellipse(positionX, height, DIAMETER, DIAMETER);
     }
 }
